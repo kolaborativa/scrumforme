c03771ff3e939240164277cf993ac7ea75167bf8
gustavox4ids <gustavo@x4ids.com.br>
2013-06-13 03:54:59 -0300

    layout sprints page

--- static/js/sprints.js
+++ static/js/sprints.js
@@ -4,30 +4,44 @@ $(".card-modal").on("click", function(event) {
 
 // click expand sprint
 $(document).on("click", ".expand_sprint", function() {
-    var item = $(this),
-        sprint_container = item.closest(".sprint_container").find(".widget-content");
-
-    sprint_container.fadeToggle("slow");
+    var item = $(this).closest(".sprint_container"),
+        element = item.find(".sprint_content");
 
     // prevent hide itens after search
     $(".story_container").show();
     $(".story_content").show();
-    item.find("i").toggleClass("icon-circle-arrow-down").toggleClass("icon-circle-arrow-up");
+    if (element.is(":visible")) {
+        element.fadeOut("slow");
+        item.find("i").removeClass("icon-circle-arrow-up").addClass("icon-circle-arrow-down");
+
+    } else{
+        element.fadeIn("slow");
+        item.find("i").removeClass("icon-circle-arrow-down").addClass("icon-circle-arrow-up");
+
+    }
+
 });
 
 // click expand story
 $(document).on("click", ".expand_story", function() {
-    var item = $(this);
-    item.closest(".widget-content").find(".story_container").fadeToggle("slow");
+    var item = $(this).closest(".sprint_content"),
+        element = item.find(".story_content");
 
-    // prevent hide itens after search
-    $(".story_content").show();
-    item.find("i").toggleClass("icon-circle-arrow-down").toggleClass("icon-circle-arrow-up");
+    if (element.is(":visible")) {
+        element.fadeOut("slow");
+        item.find("i").removeClass("icon-circle-arrow-up").addClass("icon-circle-arrow-down");
+
+    } else{
+        element.fadeIn("slow");
+        item.find("i").removeClass("icon-circle-arrow-down").addClass("icon-circle-arrow-up");
+
+    }
+
 });
 
 // click close all stories
 $(".close_all_sprints").click(function() {
-    var element = $(".widget-content");
+    var element = $(".sprint_content");
 
     // prevent hide itens after search
     if (element.is(":visible")) {
@@ -36,11 +50,12 @@ $(".close_all_sprints").click(function() {
     }
 });
 
-// expand close all stories
+// expand all stories
 $(".expand_all_sprints").click(function() {
-     var element = $(".widget-content");
+     var element = $(".sprint_content");
 
     // prevent hide itens after search
+    $(".story_container").show();
     $(".story_content").show();
     if (element.is(":hidden")) {
         element.fadeIn("slow");
@@ -51,33 +66,37 @@ $(".expand_all_sprints").click(function() {
 // livesearch
 $('input[name="livesearch"]').search('.story_content', function(on) {
     var nofound = $('#nothingfound'),
-        sprint_container = $('.widget-content'),
+        sprint_container = $('.sprint_container'),
+        sprint_content = $('.sprint_content'),
         story_container = $('.story_container'),
         story_content = $(".story_content");
 
     on.reset(function(ui) {
         nofound.hide();
-        sprint_container.hide();
-        story_container.hide();
+        sprint_container.show();
+        sprint_content.hide();
     });
 
     on.empty(function() {
         nofound.show();
-        sprint_container.hide();
-        story_container.hide();
+        sprint_container.show();
+        sprint_content.hide();
     });
 
     on.results(function(results) {
         nofound.hide();
+        sprint_container.hide();
+        sprint_content.hide();
         story_container.hide();
         var all_results = $(results),
-            results_container = all_results.closest(".story_container"),
+            results_container = all_results.closest(".sprint_container"),
             len_results = results_container.find(".story_content").length;
 
         // container show
-        all_results.closest(".widget-content").show()
-        all_results.closest(".sprint_container").find(".widget-content").show()
+        // all_results.closest(".sprint_content").show()
         results_container.show();
+        results_container.find(".sprint_content").show()
+        results_container.find(".story_container").show();
 
         // content hide
         story_content.closest(".story_content").hide();
