a85ed76753e034e999623848757db8d58f6d32f2
R4bugento <diego@x4ids.com.br>
2016-01-13 14:04:42 -0200

    CSS separates notes

--- static/js/brainstorm.js
+++ static/js/brainstorm.js
@@ -14,15 +14,14 @@ $( ".note" ).draggable({
 // by clicking the button to add Task
 $(document).on("click", ".add-note", function() {
   ajax(url.create_note + '?project_id='+ info.project_id +'&person_id='+ info.person_id, 'target_ajax');
-  //TODO: pegar informacoes dinamicamente / project_id e person_id / URL
-  //TODO: separa CSS
+  //TODO: separar CSS
   //TODO: criar a nota dinamicamente se gravar no banco antes (return status_ == true)
   //TODO: criar a nota dincamica de forma draggable
     //var definition_ready_id = $(this).closest(".item_container").attr("data-definitionready"),
         //html = '<ul class="task_container"><li class="task"><div class="avatar_container"><button class="btn btn-nostyle user_card nonuser_card choose_owner"><i class="icon-plus"></i></button></div><div class="card_container"><a href="#" class="editable-click editable-empty editable task_item new_task" data-type="textarea" data-placeholder="' + msg.field_empty + '" data-pk="' + definition_ready_id + '" data-name="task">' + msg.field_empty + '</a><div class="icons_card"><span class="delete_item icon-hover" ><i class="icon-trash"></i></span><span class="card-modal icon-hover" ><i class="icon-cog"></i></span></div></div><div class="clearfix"></div></li></ul>';
 
     // html => pegar Person name e data
-    html = '<li class="note ui-draggable ui-draggable-handle" style="list-style: none; padding: 10px; margin: 10px; position: relative; background-color: rgb(241, 196, 15);"> <p>Seu texto</p> <hr> <p>Criado por: Diego Sorrilha</p><p>Criado em: 2016-01-12</p> </li>'
+    html = '<li class="note ui-draggable ui-draggable-handle"> <p>Seu texto</p> <hr> <p>Criado por: Diego Sorrilha</p><p>Criado em: 2016-01-12</p> </li>'
 
     var newItem = $(".notes-container").prepend(html);
 
