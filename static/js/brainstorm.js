9bf7ca4aaef885ea9a78fab141b58cfc379ed1af
R4bugento <diego@x4ids.com.br>
2016-02-11 02:10:14 -0200

    refactoring

--- static/js/brainstorm.js
+++ static/js/brainstorm.js
@@ -202,8 +202,6 @@ loadDraggable = function () {
     }
   });
 
-
-
 }; // loadDraggable()
 
 
@@ -251,63 +249,6 @@ $(document).on("click", "#note-add-FAB", function() {
     }); // ajax success create
 
 
-});
-
-
-
-// Add new note
-$("#note-add").draggable({
-  containment: "#area-brainstorm",
-  stack: ".note--panel",
-  cursor: "move",
-  helper: "clone",
-  //revert: "invalid",
-   start: function(event, ui) {
-    $(ui.helper).addClass("note");
-   },
-
-  stop: function( event, ui ) {
-    var position = ui.position;
-
-    html = '<li class="note note--panel note--panel new_note ui-draggable ui-draggable-handle" data-id="" style="position: absolute; top:5; left:5">' +
-              '<div class="note-header">' +
-                '<i class="icon-note-header icon-note-header--delete icon-trash" data-pk=""></i>' +
-                '<div class="clearfix"></div>' +
-              '</div>' +
-              '<div class="note-content">' +
-                '<a class="editable-click editable" data-type="textarea" data-pk="" data-url="'+ url.update_items +'" data-type-text="note-text" style="display: inline;">'+ msg.note_default_text +'</a>' +
-              '</div>' +
-              '<div class="note-footer"></div>' +
-           '</li>';
-
-    var new_note_container = $(".notes-container").prepend(html);
-    var new_note = new_note_container.find(".new_note:first").css({"top": ui.position.top, "left": ui.position.left})[0];
-    loadDraggable();
-
-    $.ajax({
-      method: "POST",
-      url: url.create_note +'.json',
-      data: { project_id: info.project_id, person_id: info.person_id, position: JSON.stringify(position) }
-    })
-    .success(function( data ) {
-      if (data.status == false) {
-        alert(msg.create_note_error);
-        // remove the note from the DOM
-        $(new_note).fadeOut("fast", function() {
-          $(this).remove();
-        });
-      } else {
-        var btnDelete = $(new_note).find(".icon-note-header--delete")[0];
-        var link_content_note = $(new_note).find(".editable-click")[0];
-        var footer_note = $(new_note).find(".note-footer")[0];
-
-        new_note.dataset.id = data.note_id;
-        btnDelete.dataset.pk = data.note_id;
-        link_content_note.dataset.pk = data.note_id;
-        footer_note.innerHTML = data.created_at + ' - ' + data.person_name;
-      }// endif data.status
-    }); // ajax success create
-  }
 });
 
 
