03d5f90eb8237ec0ef7ada4042e55d58e4c46a36
R4bugento <diego@x4ids.com.br>
2016-02-16 20:30:07 -0200

    implements modal 'all projects'

---
+++ static/js/all-projects.js
@@ -0,0 +1,33 @@
+
+// Delete Project
+$(document).on("click", ".icon-project-card-delete", function() {
+  var this_ = this;
+  if (window.confirm('Você tem certeza que deseja continuar?')) {
+    deleteProject(this_);
+  }
+
+});
+
+function deleteProject(element) {
+  /*
+  Function that deletes a project of the database , and remove it from the DOM tree
+  */
+  var project = $(element).closest(".project-card")[0],
+      projectId = element.dataset.pk;
+  //
+  //$.ajax({
+  //  method: "POST",
+  //  url: url.remove_note +'.json',
+  //  data: { note_id: noteId }
+  //})
+  //.success(function(data) {
+  //  // remove the note from the DOM
+  //  $(note).fadeOut("fast", function() {
+  //    $(this).remove();
+  //  });
+  //});
+  alert('deletando o projeto');
+  console.log(project);
+  console.log('project_id', projectId);
+
+};
